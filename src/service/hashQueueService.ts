d76e9ff38b06f8a11bd0076b98d50fd5474cda7f
Evgeni Zharkov <zharkov.ev.u@yandex.ru>
2018-04-28 19:28:57 +0300

    Добавлены Queue-сервисы

---
+++ src/service/hashQueueService.ts
@@ -0,0 +1,48 @@
+"use strict";
+
+import {RedisClient} from "redis";
+import {promisify} from "util";
+
+let client: RedisClient;
+let hkeysAsync: (key: string) => Promise<string[]>;
+let hdelAsync: (key: string, field: string) => Promise<number>;
+let hsetAsync: (key: string, field: string, value: any) => Promise<number>;
+
+function initClient(redisClient: RedisClient) {
+  client = redisClient;
+  hkeysAsync = promisify(client.hkeys).bind(client);
+  hdelAsync = promisify(client.hdel).bind(client);
+  hsetAsync = promisify(client.hset).bind(client);
+}
+
+class HashQueueService<T> {
+  private readonly uid: string;
+  private readonly name: string;
+  private readonly hashQueueName: string;
+
+  constructor(redisClient: RedisClient, options: { uid: string, name: string }) {
+    initClient(redisClient);
+    if (!options && typeof options !== "object") {
+      throw new Error("HashQueueService init: options not a object");
+    }
+    this.uid = options.uid;
+    this.name = options.name;
+    this.hashQueueName = "hashqueue_" + this.uid + "_" + this.name;
+  }
+
+  public enqueue = async (field: string, value: T): Promise<boolean> => {
+    const result = await hsetAsync(this.hashQueueName, field, value);
+    return result === 1;
+  };
+
+  public dequeue = async (field: string): Promise<boolean> => {
+    const result = await hdelAsync(this.hashQueueName, field);
+    return result === 1;
+  };
+
+  public getFields = async (): Promise<string[]> => {
+    return hkeysAsync(this.hashQueueName);
+  };
+}
+
+export default HashQueueService;
--- src/service/queueService.ts
+++ src/service/queueService.ts
@@ -1,10 +1,31 @@
+"use strict";
+
 import {RedisClient} from "redis";
+import {promisify} from "util";
 
-class QueueService {
-  private readonly client: RedisClient;
+let client: RedisClient;
+let rpopAsync: (key: string) => Promise<number>;
+let lpushAsync: (key: string, value: any) => Promise<number>;
 
-  constructor(client: RedisClient) {
-    this.client = client;
+function initClient(redisClient: RedisClient) {
+  client = redisClient;
+  rpopAsync = promisify(client.rpop).bind(client);
+  lpushAsync = promisify(client.lpush).bind(client);
+}
+
+class QueueService<T> {
+  private readonly uid: string;
+  private readonly name: string;
+  private readonly queueName: string;
+
+  constructor(redisClient: RedisClient, options: { uid: string, name: string }) {
+    initClient(redisClient);
+    if (!options && typeof options !== "object") {
+      throw new Error("HashQueueService init: options not a object");
+    }
+    this.uid = options.uid;
+    this.name = options.name;
+    this.queueName = "queue_" + this.uid + "_" + this.name;
   }
 }
 
